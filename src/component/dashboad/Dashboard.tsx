8156cad1ed06bd3e96d435194e70e176a07fb3fc
ayomide akinrata <akinrataa@gmail.com>
2025-05-15 16:06:41 +0100

    added pie chart

--- src/component/dashboad/Dashboard.tsx
+++ src/component/dashboad/Dashboard.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { CiMenuKebab } from "react-icons/ci";
+import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
 import { FaFile } from "react-icons/fa";
+
+const data = [
+    { name: 'Orders', value: 400 },
+    { name: 'Returns', value: 100 },
+    { name: 'Revenue', value: 300 },
+    { name: 'Others', value: 200 },
+];
+
+const COLORS = ['#0088FE', '#FF8042', '#00C49F', '#FFBB28'];
 
 
 const Dashboard: React.FC = () => {
@@ -31,7 +41,7 @@ const Dashboard: React.FC = () => {
                         <p className="text-xl mt-2">Packaging</p>
                     </div>
                     <div className='mt-10 '>
-                        <p className="text-xl mt-2 w-10 h-8 bg-yellow-600 text-white flex justify-center items-center rounded-md">5</p>
+                        <p className="text-xl mt-2 w-10 h-8 bg-yellow-600 text-white flex justify-center items-center rounded-md">3</p>
                     </div>
                 </div>
 
@@ -45,7 +55,7 @@ const Dashboard: React.FC = () => {
                         <p className="text-xl mt-2">Delivered</p>
                     </div>
                     <div className='mt-10 '>
-                        <p className="text-xl mt-2 w-10 h-8 bg-blue-500 text-white flex justify-center items-center rounded-md">5</p>
+                        <p className="text-xl mt-2 w-10 h-8 bg-blue-500 text-white flex justify-center items-center rounded-md">1</p>
                     </div>
                 </div>
 
@@ -59,8 +69,35 @@ const Dashboard: React.FC = () => {
                         <p className="text-xl mt-2">Completed</p>
                     </div>
                     <div className='mt-10 '>
-                        <p className="text-xl mt-2 w-10 h-8 bg-orange-500 text-white flex justify-center items-center rounded-md">5</p>
+                        <p className="text-xl mt-2 w-10 h-8 bg-orange-500 text-white flex justify-center items-center rounded-md">1</p>
                     </div>
+                </div>
+            </div>
+            <div className=' grid lg:grid-cols-2 mt-3 gap-2 m-5 '>
+                <div className='w-auto h-96 bg-gray-700 rounded-2xl'>
+                    <h3 className='text-center text-xl my-3'>Order Status</h3>
+                    <div className="w-full flex justify-center items-center p-4">
+                        <PieChart width={300} height={300}>
+                            <Pie
+                                data={data}
+                                cx="50%"
+                                cy="50%"
+                                outerRadius={100}
+                                label
+                                dataKey="value"
+                            >
+                                {data.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                ))}
+                            </Pie>
+                            <Tooltip />
+                            <Legend />
+                        </PieChart>
+                    </div>
+
+                </div>
+                <div className='w-auto h-96 bg-gray-700 rounded-2xl'>
+                    <h3 className='text-center text-xl my-3'>Top 5 selling Product</h3>
                 </div>
             </div>
 
